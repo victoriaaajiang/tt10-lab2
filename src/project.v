350f136869c476f04f914cbd2e5a7594c19b542c
victoriaaajiang <149326113+victoriaaajiang@users.noreply.github.com>
2025-02-21 20:50:49 -0500

    added xor function in  project.v

--- src/project.v
+++ src/project.v
@@ -17,11 +17,32 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  xor bit1 (ui_in[0], uio_in[0], 1'b0, uo_out[0], uio_out[0]);
+  xor bit2 (ui_in[0], uio_in[0], 1'b0, uo_out[0], uio_out[0]);
+  xor bit3 (ui_in[0], uio_in[0], 1'b0, uo_out[0], uio_out[0]);
+  xor bit4 (ui_in[0], uio_in[0], 1'b0, uo_out[0], uio_out[0]);
+  xor bit5 (ui_in[0], uio_in[0], 1'b0, uo_out[0], uio_out[0]);
+  xor bit6 (ui_in[0], uio_in[0], 1'b0, uo_out[0], uio_out[0]);
+  xor bit7 (ui_in[0], uio_in[0], 1'b0, uo_out[0], uio_out[0]);
+  xor bit8 (ui_in[0], uio_in[0], 1'b0, uo_out[0], uio_out[0]);
 
+
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, clk, rst_n, 1'b0, uio_oe};
+
+endmodule
+
+module xor(input ded_input, 
+            input io_input, 
+            input carry_in,
+            output ded_output,
+            output carry_out);
+  assign ded_output = (~ded_input & io_input) | (ded_input & ~io_input);
+  assign carry_out = ded_input & io_input | ded_input & carry_in | io_input & carry_in;
+  
 endmodule
