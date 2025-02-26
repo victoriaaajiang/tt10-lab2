d887b9dd0fce920ee18a7c3703e7a8521bceb9c6
victoriaaajiang <149326113+victoriaaajiang@users.noreply.github.com>
2025-02-25 21:58:50 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -27,8 +27,8 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_tt10-lab2 user_project (
+
+  tt_um_third user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
